6c11a0ebd156d59e8114095adf60ed3d3ba53c56
Pulkit <297pulkitmehta@gmail.com>
2023-02-12 11:34:08 -0700

    Firestore Credentials

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import "firebase/compat/storage";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDDRSBeZuaczP8gnHIxlEtqSKQaAA96X98",
-  authDomain: "photo-gallery-b551b.firebaseapp.com",
-  projectId: "photo-gallery-b551b",
-  storageBucket: "photo-gallery-b551b.appspot.com",
-  messagingSenderId: "904586867126",
-  appId: "1:904586867126:web:ec20503eb623413d9a5117",
-  measurementId: "G-CY6QVVD0VZ",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
